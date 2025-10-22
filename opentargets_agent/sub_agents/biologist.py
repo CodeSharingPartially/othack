7eb65adf45bfd5495795b42b2d3192dca7a3a898
Paul TO <kptoemail@gmail.com>
2025-10-22 12:16:06 +0100

    Fix import error

--- opentargets_agent/sub_agents/biologist.py
+++ opentargets_agent/sub_agents/biologist.py
@@ -1,8 +1,5 @@
 from google.adk.agents import Agent
 
-# from opentargets_agent.tools.data_steward_tool import get_target_data_from_opentargets
-
-
 biologist_agent = Agent(
     model="gemini-2.5-pro",
     name="biologist",
